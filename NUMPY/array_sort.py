bfbc80e2edb5b2c1227d5e98db5ed85b48857a25
Soren Basnet <basnetsoren01@gmail.com>
2026-01-06 22:51:50 -0600

    LC94

---
+++ NUMPY/array_sort.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+
+arr = np.array([1,3,4,22,4,5,3,222,45,82,44,999,4])
+
+sorted_arr = np.sort(arr)
+
+print(f"Original arr : {arr}")
+print(f"Sorted arr : {sorted_arr}")
+
+
+
