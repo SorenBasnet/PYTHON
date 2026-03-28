4e2156bc581b2af0f1432a90a8b6004850d043a5
Soren Basnet <basnetsoren01@gmail.com>
2026-03-27 22:33:21 -0500

    file_col_names v1

--- FILE_COL_NAMES/read_file.py
+++ FILE_COL_NAMES/read_file.py
@@ -8,23 +8,28 @@ class Read_File:
     def read_file(self) -> None: 
         
         data = pd.read_csv(self.data_file_path)
+        #print(data.dtypes)
 
         dsc_file = open("dsc.txt", "w")
 
         dsc_file.write(self.data_file_path + "\n")
         
         for col_name in data.columns: 
-            if isinstance(type(col_name), int): 
-                dsc_file.write(col_name +" n" + "\n")
+            print(type(data[col_name][0]))
+
+            if pd.api.types.is_string_dtype(data[col_name]):
+                dsc_file.write(col_name + " s\n")
+
+            elif pd.api.types.is_numeric_dtype(data[col_name]):
+                dsc_file.write(col_name + " n\n")
+
+            else:
+                dsc_file.write(col_name + " o\n")
+            
 
         dsc_file.close()
 
             
-
-
-
-
-
 
 """
 Running tests
